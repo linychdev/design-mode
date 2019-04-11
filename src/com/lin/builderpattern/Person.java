b6f48b80884786b48b3c56d93ef5eba2d47d2eb6
linych <linych@thunisoft.com>
2019-04-11 19:03:48 +0800

    新增构造者模式

---
+++ src/com/lin/builderpattern/Person.java
@@ -0,0 +1,154 @@
+/*
+ * @(#)Person.java 2019年4月11日下午6:39:15
+ * design-mode
+ * Copyright 2019 Thuisoft, Inc. All rights reserved.
+ * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.lin.builderpattern;
+
+/**
+ * Person
+ * @author linych
+ * @version 1.0
+ *
+ */
+public class Person {
+    //必要参数
+    private final int id;
+    private final String name;
+    //可选参数
+    private int age;
+    private String sex;
+    private String phone;
+    private String address;
+    private String desc;
+
+    private Person(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.age = builder.age;
+        this.sex = builder.sex;
+        this.phone = builder.phone;
+        this.address = builder.address;
+        this.desc = builder.desc;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @return the sex
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @return the desc
+     */
+    public String getDesc() {
+        return desc;
+    }
+    
+    /** (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        StringBuilder builder2 = new StringBuilder();
+        builder2.append(id);
+        builder2.append(",");
+        builder2.append(name);
+        builder2.append(",");
+        builder2.append(age);
+        builder2.append(",");
+        builder2.append(sex);
+        builder2.append(",");
+        builder2.append(phone);
+        builder2.append(",");
+        builder2.append(address);
+        builder2.append(",");
+        builder2.append(desc);
+        return builder2.toString();
+    }
+
+
+
+    public static class Builder {
+        //必要参数
+        private final int id;
+        private final String name;
+        //可选参数
+        private int age;
+        private String sex;
+        private String phone;
+        private String address;
+        private String desc;
+
+        public Builder(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Builder age(int val) {
+            this.age = val;
+            return this;
+        }
+
+        public Builder sex(String val) {
+            this.sex = val;
+            return this;
+        }
+
+        public Builder phone(String val) {
+            this.phone = val;
+            return this;
+        }
+
+        public Builder address(String val) {
+            this.address = val;
+            return this;
+        }
+
+        public Builder desc(String val) {
+            this.desc = val;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+}
---
+++ src/com/lin/builderpattern/Test.java
@@ -0,0 +1,25 @@
+/*
+ * @(#)Test.java 2019年4月11日下午6:40:51
+ * design-mode
+ * Copyright 2019 Thuisoft, Inc. All rights reserved.
+ * THUNISOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.lin.builderpattern;
+
+/**
+ * Test
+ * @author linych
+ * @version 1.0
+ *
+ */
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person.Builder(1,"林轶超")
+                            .age(30)
+                            .sex("男")
+                            .desc("构造者模式")
+                            .phone("18500214725")
+                            .build();
+        System.out.println(p);
+    }
+}
